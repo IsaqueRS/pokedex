c17e1181e5a779e8d0ec28bf93a03a23abc42ab8
isaqueroberto <isaqueroberto22@gmail.com>
2022-11-11 15:41:41 -0300

    styled-pokedex

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -2,6 +2,20 @@ import React, { useEffect, useState, useTransition } from 'react';
 import './App.css'; 
 import { PokemonsFetch } from './api/services/pokemon';
 import { PokemonsImageFetch } from './api/services/pokemonImage'
+import logo from './logo.svg'
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent'; 
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import '@fontsource/roboto/500.css';
+
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  makeStyles,
+} from "@mui/material";
 
 function App() {  
   const [pokemon, setPokemon] = useState([]);
@@ -37,15 +51,32 @@ function App() {
 
   return(
     <div>
+      <AppBar position="static">
+      <CssBaseline />
+      <Toolbar sx={{ backgroundColor: 'black' }}>
+        <Typography variant="h4">
+          <img src={logo} className='img-poke'/>
+          Pokedex
+        </Typography>
+      </Toolbar>
+    </AppBar>
+
       <div className='pokedex'>
       <div className='pokemon-card-container'>
       {pokemon.map((pokemonLoop) => (
-        <div className='grid-item-pokemon'>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonLoop.entry_number}.png`}></img>  
-        <p>
-          {pokemonLoop.pokemon_species.name}
-        </p>
-        </div>
+        <Card sx={{ maxWidth: 350, backgroundColor: "endregion" }}>
+          <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonLoop.entry_number}.png`} 
+          />
+          <CardContent>
+           <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'medium', fontFamily: 'Roboto' }}>
+            {pokemonLoop.pokemon_species.name}
+           </Typography>
+          </CardContent>
+        </Card>
       ))}
       </div>
       </div>
